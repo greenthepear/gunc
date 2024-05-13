af005624a07bf5262820506d1517844d2f1e8cc0
greenthepear <green@greenthepear.com>
2024-05-13 12:59:14 +0200

    WIP Pipeline

--- other.go
+++ other.go
@@ -7,3 +7,26 @@ func Reverse[S ~[]E, E any](s S) S {
 	slices.Reverse(s)
 	return s
 }
+
+type PipeFunction[S ~[]E, E any] interface {
+	func(S) S |
+		func(S, func(E) E) S |
+		func(S, func(int, E) E) S
+}
+
+func Pipeline[S ~[]E, E any, F PipeFunction[S, E]](param S, functions ...func() S) S {
+	for _, f := range functions {
+		f1, ok := any(f).(func(S) S)
+		if ok {
+			param = f1(param)
+			continue
+		}
+		f2, ok := any(f).(func(S, func(E) E) S)
+		if ok {
+			param = 
+		}
+		f3, ok3 := any(f).(func(S, func(int, E) E) S)
+		param = f(param)
+	}
+	return param
+}
---
+++ other_test.go
@@ -0,0 +1,22 @@
+package gunc
+
+import (
+	"slices"
+	"testing"
+)
+
+func TestReverse(t *testing.T) {
+	s := Range(1, 5)
+	s = Reverse(s)
+	target := []int{5, 4, 3, 2, 1}
+	if !slices.Equal(s, target) {
+		errorTargetVsExpected(t, "reverse", s, target)
+	}
+}
+
+func TestPipeline(t *testing.T) {
+	s := Range(1, 10)
+	s = Pipeline(s,
+		Reverse,
+	)
+}
